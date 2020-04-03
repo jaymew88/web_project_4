1f477fcb77c17f5c6cacade68b7c1db3e538e2e1
Jayme Waye <jaymewaye@gmail.com>
2020-04-03 16:52:25 -0400

    feat: popup styling complete; start js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.profile__edit-btn').addEventListener("click", function() {
+	document.querySelector('.popup__opened');
+});
+
+document.querySelector('.popup__close').addEventListener("click", function() {
+	document.querySelector('.popup').style.display = "none";
+});
