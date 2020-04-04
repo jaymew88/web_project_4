aafc292777a7d3285adf7c75d270697defef14c3
Jayme Waye <jaymewaye@gmail.com>
2020-04-04 13:10:37 -0400

    feat:button fix hover and open/close popup

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector('.button__edit').addEventListener("click", function() {
-	document.querySelector('.popup').classList.add('popup__opened');
+	document.querySelector('.popup').classList.add('popup_opened');
 });
 
-document.querySelector('.popup__close').addEventListener("click", function() {
-	document.querySelector('.popup').classList.remove('popup__opened');
+document.querySelector('.button__close').addEventListener("click", function() {
+	document.querySelector('.popup').classList.remove('popup_opened');
 });
