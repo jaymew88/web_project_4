50079a8ffb3267dc8e63848400cf5940efee7deb
Jayme Waye <jaymewaye@gmail.com>
2020-04-09 08:34:56 -0400

    hotfix: button class names and porsitioning

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Calls all elemnts from document and stores them as variables
-const buttonEdit = document.querySelector('.button_edit');
+const buttonEdit = document.querySelector('.button_role_edit');
 const open = document.querySelector('.popup');
-const close = document.querySelector('.button_close');
+const close = document.querySelector('.button_role_close');
 const closeSave = document.querySelector('.button_role_save');
 const formElement = document.querySelector('.popup__form');
 const nameNew = document.querySelector('.profile__title');
@@ -10,8 +10,8 @@ const jobNew = document.querySelector('.profile__job');
 // Open Popup Modal with prefilled fields 
 buttonEdit.addEventListener("click", function() {
   open.classList.add('popup_opened'); 
-  document.querySelector('.popup__field_name').value = document.querySelector('.profile__title').innerHTML;
-  document.querySelector('.popup__field_job').value = document.querySelector('.profile__job').innerHTML;
+  document.querySelector('.popup__field_content_name').value = document.querySelector('.profile__title').innerHTML;
+  document.querySelector('.popup__field_content_job').value = document.querySelector('.profile__job').innerHTML;
 });
   
 // Closes Popup Modal when Close button, save or enter is clicked
@@ -30,8 +30,8 @@ function formSubmitHandler (evt) {
                                                 // Having done so, we can define our own way of submitting the form.
                                                 // We'll explain it in more detail later.
                                                 
-    let nameInput = document.querySelector('.popup__field_name');  
-    let jobInput = document.querySelector('.popup__field_job');  
+    let nameInput = document.querySelector('.popup__field_content_name');  
+    let jobInput = document.querySelector('.popup__field_content_job');  
     
     let nameVal = nameInput.value; 
     let jobVal = jobInput.value;
