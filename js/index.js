072a668dcc2fb3a3be8b276de6394a9b2e981e8f
Jayme Waye <jaymewaye@gmail.com>
2020-06-06 08:57:20 -0400

    hotfixes per reviewer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Imports 
-import {Card} from "./Card.js";
-import {FormValidator} from "./FormValidator.js";
+import { FormValidator } from "./FormValidator.js";
+import { Card } from "./Card.js";
 
 // Initial Cards to be loaded 
 const initialCards = [
