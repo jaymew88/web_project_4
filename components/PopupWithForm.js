b44632322ea49e8fddb7e9505455956c523b248f
Jayme Waye <jaymewaye@gmail.com>
2020-06-20 18:22:46 -0400

    feat- added PoupWithImage and PopupWithForm classes

--- components/PopupWithForm.js
+++ components/PopupWithForm.js
@@ -1,13 +1,44 @@
-// Create PopupWithForm as a child class of Popup. The class must comply with the following requirements:
+// PopupWithForm as a child class of Popup. 
+import Popup from './Popup.js';
 
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._handleSubmitForm = this.handleSubmitForm; 
+    this._form = this._popup.querySelector('.popup__form');
+    this._nameInputField = this._popup.querySelector('.popup__field_content_name');
+    this._jobInputField = this._popup.querySelector('.popup__field_content_job');
+  }
+
+  _getInputValues() {
+    return {
+      nameValue: this._nameInputField.value,
+      jobValue: this._jobInputField.value
+    }   
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', (e) => {
+      this._handleSubmitForm(e, this._getInputValues());
+      this.close();
+    });
+
+    this._form.addEventListener('click', (e) => {
+      e.stopPropagation();
+    })
+  }
+
+  close() {
+    super.close();
+    this._nameInputField.value = null;
+    this._jobInputField.value = null;
+  }
+}
 
 // 1. It takes a callback of the form submission into the constructor, as well as the popup selector.
-
-
 // 2. It stores a private method named _getInputValues(), which collects data from all the input fields.
-
 // 3. It modifies the setEventListeners() parent method. The setEventListeners() method of the PopupWithForm 
-  // class has to add the click event listener to the close icon while also adding the submit event handler.
-
+// class has to add the click event listener to the close icon while also adding the submit event handler.
 //  It modifies the close() parent method in order to reset the form once the popup is closed.
 // Create an instance of the PopupWithForm class for each popup.
--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -1,5 +1,18 @@
-// Create the PopupWithImage class as a child class of Popup. 
+// PopupWithImage class as a child class of Popup. 
+import Popup from "./Popup.js";
 
-// This class has to change the parent open() method. In the open() method of the PopupWithImage class,
-// you need to add an image to the popup and the corresponding image src attribute along with a 
-//caption for the image.
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+      super(popupSelector);
+  }
+
+  open(name, link) {
+    const image = this._popup.querySelector('.popup__img');
+    const imageName = this._popup.querySelector('.popup__img-title');
+
+    image.src = link;
+    image.alt = name;
+    imageName.textContent = name;
+    super.open();
+  }
+}
